7376e7677616ba699655083ce3faa8bcb9ff37e2
Jake Riesterer <jake@impossibleventures.com>
2015-07-14 12:35:45 -0500

    Fix delay option in browser

--- support/browser-entry.js
+++ support/browser-entry.js
@@ -119,6 +119,9 @@ mocha.ui = function(ui){
 
 mocha.setup = function(opts){
   if ('string' == typeof opts) opts = { ui: opts };
+  if (Object.prototype.hasOwnProperty.call(opts, 'delay')) {
+    this.delay(opts.delay);
+  }
   for (var opt in opts) this[opt](opts[opt]);
   return this;
 };
